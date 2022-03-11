3d1529b3e4d04ca0bc857ac198a7e3694d2c0701
johngithinji-cs <mathaijohn97@gmail.com>
2022-03-11 22:08:15 +0300

    prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers of base 10 starting from 0
+ * Description: using the main function
+ * this program prints all single digit numbers of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+{
+	printf("%i", x);
+}
+printf("\n");
+return (0);
+}
