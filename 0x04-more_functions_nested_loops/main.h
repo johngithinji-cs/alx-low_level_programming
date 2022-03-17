1d818163961c773858ae7870a8722fa0e5bc778d
johngithinji-cs <mathaijohn97@gmail.com>
2022-03-17 19:25:58 +0300

    commiting

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: holberton.h
+ * Auth: Gedeon Obae Gekonge
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x03-more_functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+
+#endif
