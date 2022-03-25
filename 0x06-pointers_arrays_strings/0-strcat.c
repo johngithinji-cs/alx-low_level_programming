ce8147e403ac8f4e8825be2ff8a296d2ec6daaba
johngithinji-cs <60788095+johngithinji-cs@users.noreply.github.com>
2022-03-25 10:30:44 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *_strcat - concatinates two strings
- *@dest:destination pointer
- *@src:pointer to a string
- *Return: concatinated string
+ * *_strcat - concatinates two strings
+ * @dest:destination pointer
+ * @src:pointer to a string
+ * Return: concatinated string
  */
 char *_strcat(char *dest, char *src)
 {
