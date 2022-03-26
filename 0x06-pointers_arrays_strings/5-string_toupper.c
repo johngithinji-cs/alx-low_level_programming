d6286572a25a0063111f0498e70ce847cc97e9fc
johngithinji-cs <60788095+johngithinji-cs@users.noreply.github.com>
2022-03-26 12:33:59 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,10 +13,10 @@ char *string_toupper(char *str)
 
 	for (index = 0; str[index] != '\0'; index++)
 	{
-		if (str[index] >= 97 && str[index] <= 122)
+		if (str[index] > 96 && str[index] < 123)
 		{
 			str - = 32;
 		}
-		return (str);
 	}
+	return (str);
 }
