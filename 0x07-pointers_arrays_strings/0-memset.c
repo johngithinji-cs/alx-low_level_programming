f8eec59927c5fa1ae89d1cb69fb15f7abe9fc07a
johngithinji-cs <mathaijohn97@gmail.com>
2022-03-29 04:41:48 +0300

    Write a function that copies memory area.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,11 +12,10 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int index;
-	unsigned char *memory = s;
-	unsigned char value = b;
+	unsigned char *memory = s, value = b;
 
 	for (index = 0; index < n; index++)
 		memory[index] = value;
+
 	return (memory);
 }
-
