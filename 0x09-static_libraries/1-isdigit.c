48fd42b99b63b75adbc5863e86ef71ed9ec4a0b6
johngithinji-cs <mathaijohn97@gmail.com>
2022-04-04 18:19:52 +0300

    final

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that prints signs of a number
+ *
+ * @c: is a digit
+ *
+ * Return: 1 if is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
