2f0470cadc00c3221608faf9e0543727fa145e88
johngithinji-cs <mathaijohn97@gmail.com>
2022-04-05 05:15:59 +0300

    returns a pointer to a newly allocated space in memory,

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,13 +13,19 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
+
 	for (i = 0; str[i]; i++)
-		i++;
-	s = malloc(sizeof(char) * (l + 1);
+		l++;
+
+	s = malloc(sizeof(char) * (l + 1));
+
 	if (s == NULL)
 		return (NULL);
+
 	for (i = 0; str[i]; i++)
 		s[i] = str[i];
-	s[i] = '\0';
+
+	s[l] = '\0';
+
 	return (s);
 }
