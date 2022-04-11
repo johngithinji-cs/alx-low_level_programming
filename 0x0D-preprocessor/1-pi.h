7f87f8e7b7e8b84d60c47414573e047340f9e195
johngithinji-cs <mathaijohn97@gmail.com>
2022-04-11 17:54:44 +0300

    header file that defines a macro named PI as an abbreviation for the token 3.14159265359.

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,11 +1,11 @@
 #ifndef PI_H
 #define PI_H
 
-/**
- * File : PI.h
- * Auth : John Mathai Githinji
- * Desc : header file that defines a macro named 
- *	PI as an abbreviation for the token 3.14159265359.
+/*
+ * File: 1-pi.h
+ * Auth: John Mathai Githinji
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
  */
 
 #define PI 3.14159265359
