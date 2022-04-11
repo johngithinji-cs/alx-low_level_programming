b23450b84c855eef2bdac18b611ab18127852d9c
johngithinji-cs <mathaijohn97@gmail.com>
2022-04-11 18:03:20 +0300

    program that prints the name of the file it was compiled from, followed by a new line

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+/**
+ * File: 2-main.c
+ * Auth: John Mathai Githinji
+ */
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file of the program
+ *        was compiled from.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s",__FILE__);
+	return (0);
+}
