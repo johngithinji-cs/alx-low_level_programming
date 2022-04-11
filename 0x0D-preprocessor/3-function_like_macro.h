83433c5290aaa12fe281745d9e5f0bfcd0497944
johngithinji-cs <mathaijohn97@gmail.com>
2022-04-11 18:28:28 +0300

    function-like macro ABS(x) that computes the absolute value of a number x

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -4,8 +4,10 @@
 /*
  * File: 3-function_like_macro.h
  * Auth: John Mathai Githinji
- * Desc: function-like macro ABS(x)
- *       that computes the absolute value of a number x
+ * Desc: Header file that defines a function-like macro ABS(x)
+ *       that computes the absolute value of a number x.
  */
+
 #define ABS(x) ((x) < 0 ? (-x) : (x))
+
 #endif
